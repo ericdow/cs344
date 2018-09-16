ae9938dde79467cf3f1c2e100e6024cf1ef98e22
Eric Dow <ericdow@alum.mit.edu>
2018-09-16 09:46:31 -0400

    fixed typo in HW2

--- HW1/HW1.cpp
+++ HW1/HW1.cpp
@@ -25,9 +25,7 @@ void preProcess(uchar4 **inputImage, unsigned char **greyImage,
                 uchar4 **d_rgbaImage, unsigned char **d_greyImage,
                 const std::string &filename) {
   //make sure the context initializes ok
-  // cudaFree(0);
-  // std::cout << cudaGetErrorString(cudaGetLastError()) << std::endl;
-  // checkCudaErrors(cudaFree(0));
+  checkCudaErrors(cudaFree(0));
 
   cv::Mat image;
   image = cv::imread(filename.c_str(), CV_LOAD_IMAGE_COLOR);
--- HW2/student_func.cu
+++ HW2/student_func.cu
@@ -265,11 +265,11 @@ void your_gaussian_blur(const uchar4 * const h_inputImageRGBA, uchar4 * const d_
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 
   //TODO: Call your convolution kernel here 3 times, once for each color channel.
-  gaussian_blur<<gridSize, blockSize>>>(d_red, d_redBlurred, numRows, numCols,
+  gaussian_blur<<<gridSize, blockSize>>>(d_red, d_redBlurred, numRows, numCols,
       d_filter, filterWidth);
-  gaussian_blur<<gridSize, blockSize>>>(d_green, d_greenBlurred, numRows, numCols,
+  gaussian_blur<<<gridSize, blockSize>>>(d_green, d_greenBlurred, numRows, numCols,
       d_filter, filterWidth);
-  gaussian_blur<<gridSize, blockSize>>>(d_blue, d_blueBlurred, numRows, numCols,
+  gaussian_blur<<<gridSize, blockSize>>>(d_blue, d_blueBlurred, numRows, numCols,
       d_filter, filterWidth);
 
   // Again, call cudaDeviceSynchronize(), then call checkCudaErrors() immediately after
